b324bb458fcbe3f2a13e9f43815aa734449295b7
deeandra <deesetyaptr@gmail.com>
2022-05-28 00:28:46 +0700

    created calorie counter prototype with js and html

---
+++ calories-burned-counter/calories.js
@@ -0,0 +1,16 @@
+async function run(){
+    const MODEL_URL = 'model.json';
+    window.model = await tf.loadLayersModel(MODEL_URL);
+    console.log(model.summary());
+}
+run();
+
+function countCaloriesBurned(weight, reps) {
+    input = tf.tensor2d([weight/100.0], [1,1])
+    result = window.model.predict(input)
+    cal_per_rep = result.dataSync()
+    cal_per_rep = Math.round(cal_per_rep[0] * 10000) / 10000
+    cal_burned = cal_per_rep * reps
+
+    return cal_burned
+}
